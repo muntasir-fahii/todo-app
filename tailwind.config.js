78932fd1484acb9968403522c72c070140345307
muntasir-fahii <fa3877957@gmail.com>
2025-08-28 01:58:34 +0600

    projert initialized

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  darkMode: "class", // ✅ important
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
